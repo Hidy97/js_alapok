ff93e25f81b3568565303d7a8793eb5c9999241b
Hidy97 <csaladi.alexandra@gmail.com>
2018-02-16 09:30:36 +0100

    Image popup, image-hidden

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 /*jslint devel: true */
+/*global $, jQuery, alert*/
+/*jslint indent: 2*/
+
+(function () {
+    'use strict';
 console.log("hello world");
-
-
 $('.image-popup').magnificPopup({
     type: 'image',
-    
+    gallery: {
+        enabled: true
+    }
 });
+}());
+
+//minden image-popup class-al rendelkező elemet elrejtünk
+// sima javascriptet használunk ehhez:
+//[].forEach.call(document.querySelectorAll('.image-popup'),
+//                function (el) {
+//    el.style.display = 'none';
+//});
+
+//ez ugyanazt csinálja mint a fentebbi kód, csa ez a jquery library segítségével:
+$('.image-popup').css('display', 'none');
